3fcd781b305bbaeb16e1eeffce97d6a576516e38
allan.johnson <allan.johnson@octanner.com>
2016-08-31 08:53:34 -0600

    initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+/* @flow */
+import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        This will be the structure in which we will build our app.
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+/* @flow */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app'
+
+ReactDOM.render(
+  <App />
+  , document.querySelector('.attach'));
